b8de69b3373151451954c6a05bc5912320d177eb
VinceZachary <36613037+VinceZachary@users.noreply.github.com>
2018-02-19 13:08:01 +0800

    Add files via upload

---
+++ Exercise_1.py
@@ -0,0 +1,7 @@
+Python 2.7.14 (v2.7.14:84471935ed, Sep 16 2017, 20:19:30) [MSC v.1500 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> money=10000000.0
+>>> nintendowii=40000.0
+>>> print('You can afford to buy', money/nintendowii, "with your money")
+('You can afford to buy', 250.0, 'with your money')
+>>> 
